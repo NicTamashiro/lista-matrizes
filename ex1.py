d9f15a20df836951f68adba2e02d33dd2c26313f
Nicolas Tamashiro <nicolas.tamashiro19@gmail.com>
2026-04-28 10:01:15 -0300

    exercicio 1 em py

--- ex1.py
+++ ex1.py
@@ -1,17 +1,19 @@
 import math
 
-num = []
+arr = []
+soma_num = 0;
 
-for i in range(0, 10):
-    n = float(input())
-    num.append(n)
+for i in range(0,10):
+    arr.append(float(input()))
+    soma_num += arr[i]
 
-media = sum(num) / len(num)
+media = soma_num / len(arr)
 
-soma_quadrado = 0
-for i in num:
-    diff = i - media
-    soma_quadrado += diff * diff
+soma_quadrado = 0;
 
-resultado = math.sqrt(soma_quadrado / len(num))
-print(resultado)
+for i in range(0,10):
+    diff = arr[i] - media
+    soma_quadrado += diff*diff
+
+resultado = math.sqrt(soma_quadrado / len(arr))
+print(f"{resultado:.3f}")
